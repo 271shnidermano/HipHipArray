5c387bbe440e3d93ead23042f7ee9606ccb5e236
Shniderman <271shnidermano@student.dist113.org>
2026-09-11 10:03:31 -0500

    Add two-dimensional array example

--- ArrayReview.java
+++ ArrayReview.java
@@ -12,8 +12,24 @@ public class ArrayReview{
 
         double[] grades = new double[5];
         for (double g : grades)
-            {
+        {
             System.out.print(g);
+        }
+        //a 2D is actually an array of arrays
+        // it is a collection of rows and columns
+
+        int[][] values = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        double[][] nums = new double[3][5];
+
+        for (int i = 0; i < values.length; i++){
+            for (int j = 0; j < values[i].length; j++){
+                System.out.print(values[i][j] + " ");
             }
+            System.out.println();
+        }
     }
 }
